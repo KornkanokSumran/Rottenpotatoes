b557b2050579db827eab5e063ecb9b8d85cf0458
Kornkanok <ec2-user@ip-172-31-45-181.us-east-2.compute.internal>
2019-09-21 05:55:19 +0000

    initial commit

---
+++ MySite/app/controllers/movies_controller.rb
@@ -0,0 +1,48 @@
+# This file is app/controllers/movies_controller.rb
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+  
+  def show
+  @movie = Movie.find_by_id(params[:id])
+  end
+  
+  def new
+    # default: render 'new' template
+  
+  end
+  
+  def create
+	  @movie = Movie.create!(movie_params)
+	  flash[:notice] = "#{@movie.title} was successfully created."
+	  redirect_to movie_path(@movie) 
+  end
+  
+  def edit
+  @movie = Movie.find params[:id]
+  end
+ 
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+  
+  def destroy
+  @movie = Movie.find(params[:id])
+  @movie.destroy
+  flash[:notice] = "Movie '#{@movie.title}' deleted."
+  redirect_to movies_path
+  end
+  
+  private
+    def movie_params
+      params.require(:movie).permit(:title, :rating, :description, :release_date)
+    end
+    
+  
+  
+ 
+end
