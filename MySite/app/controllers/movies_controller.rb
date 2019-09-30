8b7e8f7309405e50784aa3664a28cdcd53b96b3d
Kornkanok <ec2-user@ip-172-31-45-181.us-east-2.compute.internal>
2019-09-30 11:13:35 +0000

    Show Popup

--- MySite/app/controllers/movies_controller.rb
+++ MySite/app/controllers/movies_controller.rb
@@ -1,6 +1,44 @@
 # This file is app/controllers/movies_controller.rb
 
 class MoviesController < ApplicationController
+  
+  def index
+    @movies = Movie.order(:title)
+  end
+  
+  def show
+    @movie = Movie.find_by_id(params[:id])
+  end
+  
+  def new
+    # default: render 'new' template
+    @movies = Movie.new 
+  end
+  
+  def create
+	  @movie = Movie.create!(movie_params)
+	  flash[:notice] = "#{@movie.title} was successfully created."
+	  redirect_to movie_path(@movie) 
+  end
+  
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+  
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
+  
   def search_tmdb
     # hardwire to simulate failure
     Tmdb::Api.key 'a39ff553b65193a0084d55309a069293'
@@ -23,43 +61,6 @@ class MoviesController < ApplicationController
       end
     end
     redirect_to movies_path
-  end
-  
-  def index
-    @movies = Movie.all
-  end
-  
-  def show
-    @movie = Movie.find_by_id(params[:id])
-  end
-  
-  def new
-    # default: render 'new' template
-  
-  end
-  
-  def create
-	  @movie = Movie.create!(movie_params)
-	  flash[:notice] = "#{@movie.title} was successfully created."
-	  redirect_to movie_path(@movie) 
-  end
-  
-  def edit
-    @movie = Movie.find params[:id]
-  end
-
-  def update
-    @movie = Movie.find params[:id]
-    @movie.update_attributes!(movie_params)
-    flash[:notice] = "#{@movie.title} was successfully updated."
-    redirect_to movie_path(@movie)
-  end
-  
-  def destroy
-  @movie = Movie.find(params[:id])
-  @movie.destroy
-  flash[:notice] = "Movie '#{@movie.title}' deleted."
-  redirect_to movies_path
   end
   
   private
--- MySite/features/test.feature
+++ MySite/features/test.feature
@@ -1,16 +1,16 @@
 Feature: BDD test User can use rottenpotatoes
   
 Scenario: Go to Homepage
-  Given I am on the home page
+  Given I am on the RottenPotatoes home page
   When I go to the home page
   Then I should see "All Movies"
   
 Scenario: Go to Create new page
-  Given I am on the home page
+  Given I am on the RottenPotatoes home page
   When I follow "Add new movie"
   Then I should be on the Create New Movie page
   
-Scenario: Banner is real
+Scenario: Come back to Homepage
     Given I am on the Create New Movie page
     When I follow "RottenPotatoes"
     Then I should see "All Movies"
